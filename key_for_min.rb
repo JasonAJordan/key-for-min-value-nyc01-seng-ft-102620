a2c1e7886c5062b43687be2324061d4b8687214b
Jason Jordan <jasonajordan0@gmail.com>
2020-10-03 14:51:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   valuetemp = 0
   name_hash.each do|name, value|
   
-    if value < valuetemp || valuetemp = 0
+    if value < valuetemp || valuetemp == 0
       min = name
       valuetemp = value
     end 
